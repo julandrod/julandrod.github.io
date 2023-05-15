0c6355c33b34cbe81c76560f215526c064aae778
julandrod <julian.andres.rodriguez@gmail.com>
2023-05-14 22:00:48 -0500

    fix svg's height

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -24,7 +24,7 @@ const TechContainer = ({ technologies, title }) => {
                 <img
                   src={technology.icon}
                   alt={technology.name}
-                  className="w-10/12"
+                  className="w-10/12 h-full"
                 />
               </div>
               <div className="h-1/5">
