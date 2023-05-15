cbc851f71bc0826bea3b87878d5cc47ab62e09c9
julandrod <julian.andres.rodriguez@gmail.com>
2023-05-14 22:38:57 -0500

    change opacity

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -21,7 +21,7 @@ export const textVariant = (delay) => {
       hidden: {
         x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
         y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
-        opacity: 0,
+        opacity: 1,
       },
       show: {
         x: 0,
